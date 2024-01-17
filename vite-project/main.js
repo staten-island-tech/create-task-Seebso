3e65941a7cf439aeb2e5a1100558226951a81c27
Seebso <124058253+Seebso@users.noreply.github.com>
2024-01-16 20:31:21 -0500

    3

--- vite-project/main.js
+++ vite-project/main.js
@@ -22,19 +22,58 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter')) */
-let i = 0
+let i = 0;
+let factor = 0.006;
+let history = []
 const DOMSelectors = {
   button: document.querySelector('#btn'),
+  button2: document.querySelector('#btn2'),
 };
 DOMSelectors.button.addEventListener("click", function () {
-  random5();
+  oneclick();
+})
+DOMSelectors.button2.addEventListener("click", function () {
+  oneclick();
+  while (i > 0){
+    oneclick();
+  }
+})
+function oneclick(){
+  if (i >= 74){
+    factor = Math.min(1, factor + 0.06)
+  }
+  if (random5()){
+    return
+  }
   i++;
   console.log(i);
-})
+  console.log(factor);
+}
+
 function random5() {
   const a = Math.floor(Math.random() * 1000);
-  if (a === 0 || a === 1 || a === 2  || a === 3 || a === 4 || a === 5){
-    console.log("nice")
+  if (a < (factor * 1000)){
+    win();
+    return true
   }
   console.log(a)
+  return false
 }
+function win() {
+  let turns = i+1
+  console.log("Nice! You got a 5 star after " + (turns) + " pulls")
+  history.push(turns)
+  factor = 0.006
+  document.getElementById("games").innerText = history.length;
+  document.getElementById("wish").innerText = avg();
+  document.getElementById("last").innerText = turns;
+  i = 0
+}
+function avg(){
+  let sum = 0
+  for (let x of history){
+    sum = sum + x
+  }
+  return Math.round(sum / history.length * 100) / 100
+}
+// 74 6.6% 75 12.6% 76 18.6% 
